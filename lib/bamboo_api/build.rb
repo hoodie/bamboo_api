229dc485256974356cd0e4c269429cd1f8c28891
Graham Ballantyne <hello@grahamballantyne.com>
2013-09-07 16:49:19 -0700

    Add build_realtive_time to build object

--- lib/bamboo_api/build.rb
+++ lib/bamboo_api/build.rb
@@ -3,12 +3,12 @@ class BambooApi::Build
   # /result/PHO-UAT 
   attr_reader :restartable, :once_off, :continuable, :id, :number, :life_cycle_state, :state, :key,
     :link, :plan_name, :project_name, :build_started_time, :build_completed_time, :build_duration_in_seconds,
-    :revision, :vcs_revision_key, :build_test_summary, :successful_test_count, :failed_test_count,
-    :quarantined_test_count, :build_reason, :user_url, :username, :stages
+    :build_relative_time, :revision, :vcs_revision_key, :build_test_summary, :successful_test_count,
+    :failed_test_count, :quarantined_test_count, :build_reason, :user_url, :username, :stages
 
   def initialize restartable, once_off, continuable, id, number, life_cycle_state, state, key,
     link, plan_name, project_name, build_started_time, build_completed_time, build_duration_in_seconds,
-    vcs_revision_key, build_test_summary, successful_test_count, failed_test_count,
+    build_relative_time, vcs_revision_key, build_test_summary, successful_test_count, failed_test_count,
     quarantined_test_count, build_reason, stages
 
     @restartable = restartable
@@ -24,6 +24,7 @@ class BambooApi::Build
     @build_started_time = build_started_time
     @build_completed_time = build_completed_time
     @build_duration_in_seconds = build_duration_in_seconds
+    @build_relative_time = build_relative_time
     @revision = @vcs_revision_key = vcs_revision_key
     @build_test_summary = build_test_summary
     @successful_test_count = successful_test_count
@@ -80,7 +81,7 @@ class BambooApi::Build
     stages = BambooApi::Stage.parse( build )
     BambooApi::Build.new build[ "restartable" ], build[ "onceOff" ], build[ "continuable" ], build[ "id" ], build[ "number" ], build[ "lifeCycleState" ],
       build[ "state" ], build[ "key" ], build[ "link" ], build[ "planName" ], build[ "projectName" ], build[ "buildStartedTime" ], 
-      build[ "buildCompletedTime" ], build[ "buildDurationInSeconds" ], build[ "vcsRevisionKey" ], build[ "buildTestSummary" ], 
+      build[ "buildCompletedTime" ], build[ "buildDurationInSeconds" ], build["buildRelativeTime"], build[ "vcsRevisionKey" ], build[ "buildTestSummary" ], 
       build[ "successfulTestCount" ], build[ "failedTestCount" ], build[ "quarantinedTestCount" ], build[ "buildReason" ], stages
   end
 
